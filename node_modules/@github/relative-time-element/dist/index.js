d81de50b2ccdad3a5be16ddb5d81a366a74089bb
강태욱 <twkang@mantech.co.kr>
2023-06-20 15:23:00 +0900

    minify app

--- node_modules/@github/relative-time-element/dist/index.js
+++
@@ -1,4 +0,0 @@
-import RelativeTimeElement from './relative-time-element-define.js';
-export { RelativeTimeElement };
-export default RelativeTimeElement;
-export * from './relative-time-element-define.js';
